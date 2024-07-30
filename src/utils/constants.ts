a8cdeb76582a7673634ed7ca3d4f5c209821087e
Francisco Zavalla <franzavalla85@gmail.com>
2024-07-30 14:37:35 -0300

    feat: Improved not found message on close (#26)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,5 +11,6 @@ export const ONE_DAY_MS = 24 * 60 * 60 * 1000;
 export const BOT_CODES = {
   BOUNTY_ALREADY_EXIST: "BountyAlreadyExist",
   BOUNTY_TAKEN: "BountyTaken",
-  BOUNTY_NOT_FOUND: "BountyNotFound"
+  BOUNTY_NOT_FOUND: "BountyNotFound",
+  CLOSE_ACTION_NOT_FOUND: "CloseActionNotFound"
 };
