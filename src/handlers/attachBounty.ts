b43005d585e43ae6eb403405a9dbb42b3dc244c1
FranZavalla <franzavalla85@gmail.com>
2024-08-02 17:12:14 -0300

    fixed: deadline in attach message

--- src/handlers/attachBounty.ts
+++ src/handlers/attachBounty.ts
@@ -70,7 +70,7 @@ export async function attachBounty(
         address,
         amount,
         bountyId: bounty.id,
-        deadline,
+        deadline: Date.now() + deadline_ut,
         signUrl,
         isDev: network === "preprod"
       })
